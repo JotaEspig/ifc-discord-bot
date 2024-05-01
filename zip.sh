2ac904bdecd7169fa687310a91c8cb55cba4587a
João Vitor Espig <jotaespig@gmail.com>
2024-04-30 22:57:32 -0300

    fix: zip script

--- zip.sh
+++ zip.sh
@@ -1,1 +1,2 @@
-zip -r ifcbot.zip . -x venv\* -x .git\* -x **/__pycache__\*
+rm ifcbot.zip 2> /dev/null
+zip -r ifcbot.zip discloud.config ifcbot main.py requirements.txt token.config -x venv\* -x .git\* -x **/__pycache__\* -x "*.db"
